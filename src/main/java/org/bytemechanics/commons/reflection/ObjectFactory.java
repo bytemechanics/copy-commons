75117b5444ce9a2df48cff922ac8e738f13eb5b4
afarre <afarre@CROM>
2018-02-19 00:07:46 +0100

    added objectfactory class

---
+++ src/main/java/org/bytemechanics/commons/reflection/ObjectFactory.java
@@ -0,0 +1,163 @@
+package org.bytemechanics.commons.reflection;
+
+/*
+ * Copyright 2017 Byte Mechanics.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Factory to create objects looking for the appropiate constructor
+ * @author afarre 
+ * @param <T> Type of class to instantiate
+ * @since 1.1.0
+ */
+public class ObjectFactory<T> {
+
+	private final Class<T> toInstantiate;
+	private final Object[] attributes;
+	
+	
+	protected ObjectFactory(final Class<T> _class){
+		this(_class,new Object[0]);
+	}
+	protected ObjectFactory(final Class<T> _class,final Object... _attributes){
+		this.toInstantiate=_class;
+		this.attributes=_attributes;
+	}
+
+	
+	/**
+	 * Returns the class to instantiate
+	 * @return class to instantiate
+	 */
+	public Class<T> getToInstantiate() {
+		return toInstantiate;
+	}
+	/**
+	 * Attributes to use for instantiation
+	 * @return List of attributes to use for instantiation
+	 */
+	public List<Object> getAttributes() {
+		return Stream.of(this.attributes)
+					.collect(Collectors.toList());
+	}
+	
+	
+	private Class autobox(final Class _class){
+	
+		Class reply=_class;
+		
+		if(_class.isPrimitive()){
+			if(void.class.equals(_class)){
+				reply=Void.class;
+			}else if(byte.class.equals(_class)){
+				reply=Byte.class;
+			}else if(boolean.class.equals(_class)){
+				reply=Boolean.class;
+			}else if(char.class.equals(_class)){
+				reply=Character.class;
+			}else if(short.class.equals(_class)){
+				reply=Short.class;
+			}else if(int.class.equals(_class)){
+				reply=Integer.class;
+			}else if(long.class.equals(_class)){
+				reply=Long.class;
+			}else if(float.class.equals(_class)){
+				reply=Float.class;
+			}else if(double.class.equals(_class)){
+				reply=Double.class;
+			}
+		}
+		
+		return reply;
+	}
+
+	private boolean assignableArray(final Class[] _destiny,final Object[] _origin){
+	
+		boolean reply=true;
+	
+		for(int ic1=0;ic1<_destiny.length;ic1++){
+			final Class boxedDestiny=autobox(_destiny[ic1]);
+			final Class boxedOrigin=autobox(_origin[ic1].getClass());
+			reply&=boxedDestiny.isAssignableFrom(boxedOrigin);
+		}
+		
+		return reply;
+	}	
+	private <T> Optional<Constructor<T>> findConstructor(){
+		
+		return Stream.of(this.toInstantiate.getConstructors())
+				.filter(selectedConstructor -> selectedConstructor.getParameterTypes().length==this.attributes.length)
+				.filter(selectedConstructor -> assignableArray(selectedConstructor.getParameterTypes(),this.attributes))
+				.map(selectedConstructor -> (Constructor<T>)selectedConstructor)
+				.findAny();
+	}
+	
+	/**
+	 * Static method to retrieve object factory for the given class
+	 * @param <TYPE> type of the object factory
+	 * @param _class class to produce the factory
+	 * @return ObjectFactory for this class
+	 */
+	public static final <TYPE> ObjectFactory<TYPE> of(final Class<TYPE> _class){
+		return new ObjectFactory<>(_class);
+	}
+	/**
+	 * Attributes to use when instance the object
+	 * @param _attributes attributes to use when instance the object
+	 * @return an object factory from the current class and with the given attributes to use on instance
+	 */
+	public final ObjectFactory<T> with(final Object... _attributes){
+		return new ObjectFactory<>(this.toInstantiate,_attributes);
+	}
+
+	/**
+	 * Returns the supplier for the class of this factory using, if provided, the attributes
+	 * @return Supplier for the current factory class
+	 * @see Supplier
+	 */
+	public Supplier<Optional<T>> supplier(){
+		
+		return () -> findConstructor()
+						.map(constructor -> {
+
+							T reply=null;
+
+							try{
+								reply=(T)constructor.newInstance(this.attributes);
+							} catch (SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+								Logger.getLogger(ObjectFactory.class.getName())
+									  .log(Level.SEVERE, e, () -> Optional.ofNullable(this.attributes)
+																			.map(Arrays::asList)
+																			.map(attributesList -> MessageFormat.format("Unable to instantiate object using constructor {0} with attributes {1}",constructor,attributesList))
+																			.orElseGet(() -> MessageFormat.format("Unable to instantiate object using constructor {0} without arguments",constructor)));
+							}
+
+							return reply;
+						});
+	}	
+}
---
+++ src/test/groovy/org/bytemechanics/commons/reflection/ObjectFactorySpec.groovy
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2017 Byte Mechanics.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bytemechanics.commons.reflection
+
+import org.bytemechanics.commons.reflection.ObjectFactory;
+import org.bytemechanics.commons.reflection.DummieServiceImpl;
+import java.util.logging.*
+import spock.lang.Specification;
+import spock.lang.Unroll
+import java.util.logging.*
+
+
+/**
+ *
+ * @author afarre
+ */
+class ObjectFactorySpec extends Specification{
+	
+	def setupSpec(){
+		println(">>>>> ObjectFactorySpec >>>> setupSpec")
+		final InputStream inputStream = ObjectFactorySpec.class.getResourceAsStream("/logging.properties");
+		try{
+			LogManager.getLogManager().readConfiguration(inputStream);
+		}catch (final IOException e){
+			Logger.getAnonymousLogger().severe("Could not load default logging.properties file");
+			Logger.getAnonymousLogger().severe(e.getMessage());
+		}finally{
+			if(inputStream!=null)
+				inputStream.close();
+		}
+	}
+	
+	
+	def setup(){
+		println(">>>>> ObjectFactorySpec >>>> setup")
+		Handler ch = new ConsoleHandler();
+		ch.setLevel(Level.ALL)
+		Logger.getLogger("org.bytemechanics.service.repository.internal.ObjectFactory").addHandler(ch)
+		Logger.getLogger("org.bytemechanics.service.repository.internal.ObjectFactory").setLevel(Level.ALL)		
+	}
+	
+	@Unroll
+	def "Calling of(#objectiveClass) should return an instance of ObjectFactory with instantiation objective #objectiveClass"(){
+		println(">>>>> ObjectFactorySpec >>>> Calling of($objectiveClass) should return an instance of ObjectFactory with instantiation objective $objectiveClass")
+
+		when:
+			def objectFactory=ObjectFactory.of(objectiveClass)
+
+		then:
+			objectFactory!=null
+			objectFactory instanceof ObjectFactory
+			objectFactory.getToInstantiate()!=null
+			objectFactory.getToInstantiate()==objectiveClass
+			
+		where:
+			objectiveClass << [DummieServiceImpl.class]
+	}
+	
+	@Unroll
+	def "Calling with(#arguments) over an existent ObjectFactory of #objectiveClass should return an instance of ObjectFactory with instantiation objective #objectiveClass with #arguments constructor"(){
+		println(">>>>> ObjectFactorySpec >>>> Calling with($arguments) over an existent ObjectFactory of $objectiveClass should return an instance of ObjectFactory with instantiation objective $objectiveClass with $arguments constructor")
+
+		when:
+			def objectFactory=ObjectFactory.of(objectiveClass)
+										.with((Object[])arguments)
+
+		then:
+			objectFactory!=null
+			objectFactory instanceof ObjectFactory
+			objectFactory.getToInstantiate()!=null
+			objectFactory.getToInstantiate()==objectiveClass
+			objectFactory.getAttributes()!=null
+			objectFactory.getAttributes()==arguments
+			
+		where:
+			objectiveClass			| arguments
+			DummieServiceImpl.class	| []
+			DummieServiceImpl.class	| ["1arg-arg1"]
+			DummieServiceImpl.class	| ["1arg-arg1",3,"3arg-arg2"]
+	}
+	
+	@Unroll
+	def "Search constructor over ObjectFactory instance of #objectiveClass with #arguments should return an optional of #constructor"(){
+		println(">>>>> ObjectFactorySpec >>>> Search constructor over ObjectFactory instance of $objectiveClass with $arguments should return an optional of $constructor")
+
+		when:
+			def optionalConstructor=ObjectFactory.of(objectiveClass)
+										.with((Object[])arguments)
+										.findConstructor()
+
+		then:
+			optionalConstructor!=null
+			optionalConstructor instanceof Optional
+			optionalConstructor.isPresent()
+			constructor==optionalConstructor.get()
+			
+		where:
+			objectiveClass			| arguments						| constructor
+			DummieServiceImpl.class	| []							| DummieServiceImpl.class.getConstructor()
+			DummieServiceImpl.class	| ["1arg-arg1"]					| DummieServiceImpl.class.getConstructor((Class[])[String.class])
+			DummieServiceImpl.class	| ["1arg-arg1",3,"3arg-arg2"]	| DummieServiceImpl.class.getConstructor((Class[])[String.class,int.class,String.class])
+	}
+
+	@Unroll
+	def "when object factory builds a supplier of #supplierClass with #arguments should return a supplier of #supplierClass"(){
+		println(">>>>> ObjectFactorySpec >>>> when object factory builds a supplier of $supplierClass with $arguments should return a supplier of $supplierClass")
+
+		when:
+			def supplier=ObjectFactory.of(supplierClass)
+										.with((Object[])arguments)
+										.supplier()
+			def instance=(supplier!=null)? supplier.get().get() : null
+			def args=(instance!=null)? [instance.getArg1(),instance.getArg2(),instance.getArg3()] : null
+
+		then:
+			supplier!=null
+			instance!=null
+			supplierClass.equals(instance.getClass())
+			expected==args
+			
+		where:
+			supplierClass			| arguments
+			DummieServiceImpl.class	| []
+			DummieServiceImpl.class	| ["1arg-arg1"]
+			DummieServiceImpl.class	| ["1arg-arg1",3,"3arg-arg2"]
+			expected=[(arguments.size()>0)? arguments[0] : "",(arguments.size()>1)? arguments[1] : 0,(arguments.size()>2)? arguments[2] : ""]			
+		}
+
+	@Unroll
+	def "when object factory builds a supplier of #supplierClass with #arguments should return a empty optional and write a log"(){
+		println(">>>>> ObjectFactorySpec >>>> when object factory builds a supplier of $supplierClass with $arguments should return a empty optional and write a log")
+
+		when:
+			def result=ObjectFactory.of(supplierClass)
+										.with((Object[])arguments)
+										.supplier()
+										.get()
+
+		then:
+			result!=null
+			!result.isPresent()
+			
+		where:
+			supplierClass			| arguments
+			DummieServiceImpl.class	| ["1arg-arg1",3,false,"3arg-arg2"]
+	}
+}
+
